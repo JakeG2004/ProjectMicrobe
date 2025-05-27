e18dd85cc78abd82f04eec7cc38c933c9e93a18f
Jake Gendreau <jake.lgendreau@gmail.com>
2025-05-27 16:40:10 -0700

    Added pause menu, making character creator functional again

---
+++ Assets/Scripts/UI Scripts/PuaseMenu/PauseMenu.cs
@@ -0,0 +1,42 @@
+// PuaseMenu.cs
+// A script which manages the pause menu
+// Author:  Jake Gendreau
+// Date:    5/27/25 (SBR came out in English :)))) )
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool _isActive = false;
+
+    [SerializeField] private GameObject _menu;
+
+    void Start()
+    {
+        _menu.SetActive(false);
+    }
+
+    public void TogglePause()
+    {
+        _isActive = !_isActive;
+
+
+        _menu.SetActive(_isActive);
+
+        // Freeze camera controls
+        GetComponent<ShowHideMouse>().SetState(_isActive);
+        GetComponent<ToggleCameraTracking>().SetCameraTracking(!_isActive);
+    }
+
+    public void ReloadLevel()
+    {
+        LevelLoader.Instance.ReloadLevel();
+    }
+
+    public void ExitLevel()
+    {
+        LevelLoader.Instance.LoadLevel("MainMenu");
+    }
+}
